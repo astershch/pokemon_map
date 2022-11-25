adb8ad81b65013a2d8a96e0f94aebe1633f8964f
astershch <107745329+astershch@users.noreply.github.com>
2022-11-25 22:25:39 +0600

    set required and optional fields in models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,8 +3,18 @@ from django.db import models  # noqa F401
 # your models here
 class Pokemon(models.Model):
     title = models.CharField('Имя', max_length=200)
-    title_en = models.CharField('Имя анг.', max_length=200)
-    title_jp = models.CharField('Имя яп.', max_length=200)
+    title_en = models.CharField(
+        'Имя анг.',
+        max_length=200,
+        null=True,
+        blank=True,
+    )
+    title_jp = models.CharField(
+        'Имя яп.',
+        max_length=200,
+        null=True,
+        blank=True,
+    )
     description = models.TextField('Описание')
     image = models.ImageField('Изображение', upload_to='images', null=True, blank=True)
     previous_evolution = models.ForeignKey(
@@ -28,8 +38,33 @@ class PokemonEntity(models.Model):
     lon = models.FloatField('Долгота')
     appeared_at = models.DateTimeField('Появится', default=None)
     disappeared_at = models.DateTimeField('Исчезнет', default=None)
-    level = models.IntegerField('Уровень', default=0)
-    health = models.IntegerField('Здоровье', default=0)
-    strength = models.IntegerField('Сила', default=0)
-    defence = models.IntegerField('Защита', default=0)
-    stamina = models.IntegerField('Выносливость', default=0)
+    level = models.IntegerField(
+        'Уровень',
+        default=0,
+        null=True,
+        blank=True,
+    )
+    health = models.IntegerField(
+        'Здоровье',
+        default=0,
+        null=True,
+        blank=True,
+    )
+    strength = models.IntegerField(
+        'Сила',
+        default=0,
+        null=True,
+        blank=True,
+    )
+    defence = models.IntegerField(
+        'Защита',
+        default=0,
+        null=True,
+        blank=True,
+    )
+    stamina = models.IntegerField(
+        'Выносливость',
+        default=0,
+        null=True,
+        blank=True,
+    )
