6ccdce4329a933cab9ddfaa246acb09845cb0676
astershch <107745329+astershch@users.noreply.github.com>
2022-11-25 22:20:36 +0600

    add localization to models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,13 +2,14 @@ from django.db import models  # noqa F401
 
 # your models here
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200)
-    title_jp = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField(upload_to='images', null=True, blank=True)
+    title = models.CharField('Имя', max_length=200)
+    title_en = models.CharField('Имя анг.', max_length=200)
+    title_jp = models.CharField('Имя яп.', max_length=200)
+    description = models.TextField('Описание')
+    image = models.ImageField('Изображение', upload_to='images', null=True, blank=True)
     previous_evolution = models.ForeignKey(
         'self',
+        verbose_name='В кого эволюционирует',
         null=True,
         blank=True,
         on_delete=models.SET_NULL
@@ -18,13 +19,17 @@ class Pokemon(models.Model):
         return self.title
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    lat = models.FloatField()
-    lon = models.FloatField()
-    appeared_at = models.DateTimeField(default=None)
-    disappeared_at = models.DateTimeField(default=None)
-    level = models.IntegerField(default=0)
-    health = models.IntegerField(default=0)
-    strength = models.IntegerField(default=0)
-    defence = models.IntegerField(default=0)
-    stamina = models.IntegerField(default=0)
+    pokemon = models.ForeignKey(
+        Pokemon,
+        verbose_name='Покемон',
+        on_delete=models.CASCADE,
+    )
+    lat = models.FloatField('Широта')
+    lon = models.FloatField('Долгота')
+    appeared_at = models.DateTimeField('Появится', default=None)
+    disappeared_at = models.DateTimeField('Исчезнет', default=None)
+    level = models.IntegerField('Уровень', default=0)
+    health = models.IntegerField('Здоровье', default=0)
+    strength = models.IntegerField('Сила', default=0)
+    defence = models.IntegerField('Защита', default=0)
+    stamina = models.IntegerField('Выносливость', default=0)
