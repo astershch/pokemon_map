b286131d90f8173d2a5c9c77e2e1153519376641
astershch <107745329+astershch@users.noreply.github.com>
2022-11-25 16:29:49 +0600

    add datetime fields to PokemonEntity

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,4 +1,5 @@
 from django.db import models  # noqa F401
+from django.utils.timezone import now
 
 # your models here
 class Pokemon(models.Model):
@@ -12,3 +13,5 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     lat = models.FloatField()
     lon = models.FloatField()
+    appeared_at = models.DateTimeField(default=now)
+    disappeared_at = models.DateTimeField(default=now)
